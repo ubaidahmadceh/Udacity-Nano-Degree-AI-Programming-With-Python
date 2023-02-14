c5e63b3411b76ab7ad5b09a2458cc4cd0ef33be3
Ubaid Ahmad <ubaidahmadceh@gmail.com>
2023-02-14 15:07:59 +0500

    add

---
+++ helping_codes_udacity_nano_degree/values_with_e.py
@@ -0,0 +1,64 @@
+# 4.445e8 is equal to 4.445 * 10 ** 8 which is equal to 444500000.0
+
+x = 5e6
+x -= x*(10/100)
+print(x)
+
+
+# The current volume of a water reservoir (in cubic metres)
+reservoir_volume = 4.445e8
+# The amount of rainfall from a storm (in cubic metres)
+rainfall = 5e6
+
+# decrease the rainfall variable by 10% to account for runoff
+rainfall  -= rainfall*(10/100)
+
+# add the rainfall variable to the reservoir_volume variable
+reservoir_volume += rainfall
+
+# increase reservoir_volume by 5% to account for stormwater that flows
+# into the reservoir in the days following the storm
+reservoir_volume += reservoir_volume*(5/100)
+
+# decrease reservoir_volume by 5% to account for evaporation
+reservoir_volume -= reservoir_volume*(5/100)
+
+# subtract 2.5e5 cubic metres from reservoir_volume to account for water
+# that's piped to arid regions.
+reservoir_volume -= 2.5e5
+
+# print the new value of the reservoir_volume variable
+print(reservoir_volume)
+
+
+
+
+
+
+
+
+# udacity solution
+# The current volume of a water reservoir (in cubic metres)
+reservoir_volume = 4.445e8
+# The amount of rainfall from a storm (in cubic metres)
+rainfall = 5e6
+
+# decrease the rainfall variable by 10% to account for runoff
+rainfall *= .9
+
+# add the rainfall variable to the reservoir_volume variable
+reservoir_volume += rainfall
+
+# increase reservoir_volume by 5% to account for stormwater that flows
+# into the reservoir in the days following the storm
+reservoir_volume *= 1.05
+
+# decrease reservoir_volume by 5% to account for evaporation
+reservoir_volume *= 0.95
+
+# subtract 2.5e5 cubic metres from reservoir_volume to account for water
+# that's piped to arid regions.
+reservoir_volume -= 2.5e5 
+
+# print the new value of the reservoir_volume variable
+print(reservoir_volume)
